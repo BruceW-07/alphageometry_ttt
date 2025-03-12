b679c5c22c8d70d49427a2fe6922b6e993d314ea
tttcat@redmibook <2399512924@qq.com>
2025-03-12 09:03:12 +0800

    1

---
+++ gen_data.py
@@ -0,0 +1,44 @@
+import graph as gh
+import random as rd
+import problem as pr
+from absl import flags
+
+ClauseNumMin = 2
+ClauseNumMax = 7
+ConstructNumMin = 1
+ConstructNumMax = 3
+
+_DEFS_FILE = flags.DEFINE_string(
+    'defs_file',
+    'defs.txt',
+    'definitions of available constructions to state a problem.',
+)
+_RULES_FILE = flags.DEFINE_string(
+    'rules_file', 'rules.txt', 'list of deduction rules used by DD.'
+)
+
+def main():
+  global DEFINITIONS
+  global RULES
+
+  # definitions of terms used in our domain-specific language.
+  DEFINITIONS = pr.Definition.from_txt_file(_DEFS_FILE.value, to_dict=True)
+  # load inference rules used in DD.
+  RULES = pr.Theorem.from_txt_file(_RULES_FILE.value, to_dict=True)
+
+  g = gh.Graph()
+  clause_num = rd.randint(ClauseNumMin, ClauseNumMax)
+  for i in range(clause_num):
+    flag = False
+    while not flag:
+      cdef = rd.choice(list(DEFINITIONS.values()))
+      if len(cdef.args) <= len(g.type2nodes['Point']):
+        points = rd.sample(g.type2nodes['Point'], len(cdef.args))
+        mapping = dict(zip(cdef.construction.args, points))
+        flag = True
+        for d in cdef.deps.constructions:
+          args = [mapping[arg] for arg in d.args]
+          if not g.check(d.name, args):
+            flag = False
+            break
+      
