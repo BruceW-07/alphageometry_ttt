c6190229fb356555d282b82aac3599415c47d47a
ttt <3220100013@zju.edu.cn>
2025-03-17 17:59:47 +0800

    1

--- gen_data.py
+++ gen_data.py
@@ -35,7 +35,9 @@ def main():
       cdef = rd.choice(list(DEFINITIONS.values()))
       if len(cdef.args) > len(g.type2nodes['Point']):
         continue
+      # 从已有点中随机选一些点作为新 construction 的参数
       points = rd.sample(g.type2nodes['Point'], len(cdef.args))
+      # 建立从 args 到 points 的就映射
       mapping = dict(zip(cdef.construction.args, points))
       flag = True
       for d in cdef.deps.constructions:
