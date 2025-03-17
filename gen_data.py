a2ee1b474d771b3b126e77a880adb0e2d76a087a
tttcat@redmibook <2399512924@qq.com>
2025-03-17 22:21:01 +0800

    1

--- gen_data.py
+++ gen_data.py
@@ -1,12 +1,13 @@
 import graph as gh
 import random as rd
 import problem as pr
+import time
 from absl import flags
 
 ClauseNumMin = 2
 ClauseNumMax = 7
 ConstructNumMin = 1
-ConstructNumMax = 3
+ConstructNumMax = 2
 
 _DEFS_FILE = flags.DEFINE_string(
     'defs_file',
@@ -50,7 +51,36 @@ def main():
       if not flag:
         continue
       
-      
+      new_points = cdef.points
+      constructions = [cdef.construction]
+      cons_num = rd.randint(1, ClauseNumMax)
+
+      for i in range(cons_num - 1):
+        flag = 0
+        start_time = time.time()
+        while not flag: 
+          cdef = rd.choice(list(DEFINITIONS.values()))
+          if len(cdef.points) != len(new_points):
+            continue
+          points = rd.sample(g.type2nodes['Point'], len(cdef.args))
+          mapping = dict(zip(cdef.construction.args, points))
+          flag = True
+          for d in cdef.deps.constructions:
+            args = [mapping[arg] for arg in d.args]
+            if not g.check(d.name, args):
+              flag = False
+              break
+          
+          if not flag:
+            continue
+          else:
+            constructions.append(cdef)
+            break
+
+          if time.time() - start_time > 1:  # 1 second timeout
+            break
+            
+        
       
 
       
