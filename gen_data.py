af29ab82254f9f2e9a642390f673f7d21db88643
tttcat@redmibook <2399512924@qq.com>
2025-03-18 11:45:14 +0800

    初步构建采样代码

--- gen_data.py
+++ gen_data.py
@@ -3,6 +3,7 @@ import random as rd
 import problem as pr
 import time
 from absl import flags
+from absl import app
 
 ClauseNumMin = 2
 ClauseNumMax = 7
@@ -18,7 +19,7 @@ _RULES_FILE = flags.DEFINE_string(
     'rules_file', 'rules.txt', 'list of deduction rules used by DD.'
 )
 
-def main():
+def main(_):
   global DEFINITIONS
   global RULES
 
@@ -28,13 +29,19 @@ def main():
   RULES = pr.Theorem.from_txt_file(_RULES_FILE.value, to_dict=True)
 
   g = gh.Graph()
+  added = []
+  plevel = 0
+  clauses = []
   clause_num = rd.randint(ClauseNumMin, ClauseNumMax)
   for i in range(clause_num):
     # 找一个 clause
     flag = False
+    new_points = []
+    constructions = []
     while not flag:
       # 先找到第一个 construction (确认新点)
       cdef = rd.choice(list(DEFINITIONS.values()))
+      print(cdef.construction.name)
       if len(cdef.args) > len(g.type2nodes['Point']):
         continue
       # 从已有点中随机选一些点作为新 construction 的参数
@@ -47,22 +54,24 @@ def main():
         if not g.check(d.name, args):
           flag = False
           break
-      # 如果 deps 不满足, 则继续随机
-      if not flag:
-        continue
       
-      new_points = cdef.points
-      constructions = [cdef.construction]
+      if flag:
+        new_points = cdef.points
+        constructions.append(cdef.construction)
+    else:
       cons_num = rd.randint(1, ClauseNumMax)
 
       # 如果 cdef 中没有 args (即纯构造类的 construction), 则只选择一个 construction
       if len(cdef.args) == 0:
         cons_num = 1
 
+      # 对当前 clause 找每一个 construction
       for i in range(cons_num - 1):
         flag = 0
         start_time = time.time()
         while not flag: 
+          if time.time() - start_time > 1:  # 1 second timeout
+            break
           cdef = rd.choice(list(DEFINITIONS.values()))
           if len(cdef.points) != len(new_points):
             continue
@@ -75,16 +84,21 @@ def main():
               flag = False
               break
           
-          if not flag:
-            continue
-          else:
-            constructions.append(cdef)
-            break
+          if flag:
+            constructions.append(cdef.construction)
 
-          if time.time() - start_time > 1:  # 1 second timeout
-            break
+      new_clause = pr.Clause(new_points, constructions)
+      adds, plevel = g.add_clause(
+        new_clause, plevel, DEFINITIONS,
+      )
+      added += adds
+      clauses.append(new_clause)
+    
+  g.plevel = plevel
 
-        
-      
-
+  for clause in clauses:
+    print(clause, end='; ')
+  
+if __name__ == '__main__':
+  app.run(main)
       
